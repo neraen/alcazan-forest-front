1729f0f69c21b4f460007af80ad2909cefb57b0b
noxicia <clement.silvestre31@gmail.com>
2022-10-18 08:04:00 +0200

    ajout barre sort profil spell

--- src/components/profil/ProfilSpells.jsx
+++ src/components/profil/ProfilSpells.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import UsersApi from "../../services/UsersApi";
+import ProfilSpellBar from "../UserInterface/ProfilSpellBar";
 
 
 const ProfilSpells = (props) => {
@@ -8,7 +9,7 @@ const ProfilSpells = (props) => {
 
     useEffect(() => {
         fetchAllPlayerSpell()
-    })
+    }, [])
 
     const fetchAllPlayerSpell = async() =>{
         const spellsFromApi = await UsersApi.getPlayerSpells()
@@ -16,21 +17,31 @@ const ProfilSpells = (props) => {
     }
 
     return <>
-      <h1>Profils spells</h1>
+        <h1 className="profil-spells-title">Vos sortilèges</h1>
         <table className="profil-spells-container">
             <tr className="spell-detail-container">
                 <th>Sortilege</th>
                 <th>Description</th>
                 <th>Ordre d'affichage</th>
             </tr>
-            {spells && spells.map(spell => (
+            {spells && spells.map((spell, index) => (
                 <tr className="spell-detail-container">
-                    <td>{spell.name}</td>
-                    <td>{spell.description} /// {spell.cooldown} /// {spell.portee}</td>
-                    <td>1</td>
+
+                    <td>{spell.nom}</td>
+                    <td>
+                        <div className="spell-description-cell">
+                            <img src={"../../../img/spell/" + spell.icone} className="spell-description-img img-spell"/>
+                            <div className="spell-description-text">
+                                {spell.description} <br /> Cooldown : {spell.cooldown} <br /> Portée : {spell.portee}
+                            </div>
+                        </div>
+                    </td>
+                    <td><input className="spell-order-input form-control" type="number" value={index}/></td>
                 </tr>
             ))}
         </table>
+
+        {spells && <ProfilSpellBar spells={spells}/>}
     </>
 }
 
