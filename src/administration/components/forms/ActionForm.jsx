da62f8534aeaf4de2fa8b0f8458641d6bae171a5
neraen <clement.silvestre31@gmail.com>
2023-01-08 01:02:35 +0100

    wip quest maker actions objets

--- src/administration/components/forms/ActionForm.jsx
+++ src/administration/components/forms/ActionForm.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import '../../../styles/app.css'
 import actionTypeApi from "../../services/actionTypeApi";
 import Field from "../../../components/forms/Field";
+import Select from "../../../components/forms/Select";
+import objectApi from "../../../services/objectApi";
+
 
 class ActionForm extends React.Component{
     constructor(props) {
@@ -21,6 +24,12 @@ class ActionForm extends React.Component{
     fetchAllFieldsAndValues = async () =>{
         const fields = await actionTypeApi.getAllFields(this.props.action.actionTypeId);
         this.setState({fields: fields})
+        if(this.props.action.actionTypeName === "donnerObjet"){
+            const objets = await objectApi.getAllObjects();
+            this.setState({fieldContent: objets})
+        }
+
+        console.log(this.state.fieldContent)
     }
 
     handleChange = (event) => {
@@ -34,8 +43,17 @@ class ActionForm extends React.Component{
             <div className="action-container">
                 <h6>{this.state.actionName + " : " + this.props.action.actionTypeName}</h6>
                 <Field name="actionName" value={this.state.actionName} onChange={this.handleChange} label="Nom de l'action"/>
-                {this.state.fields && this.state.fields.length > 0 && this.state.fields.map((field, index) => {
-                    return <Field name={field.name} type={field.type} label={field.name} value={field.value} onChange={this.handleSequenceChange}/>
+                {this.state.fields && this.state.fields.length > 0 && this.state.fields.map((field) => {
+                    if(field.type === "select"){
+                        return <Select name={field.name} onChange={this.handleChange} label={field.name}>
+                            {this.state.fieldContent && this.state.fieldContent.length > 0 && this.state.fieldContent.map((content, index) => {
+                                return <option key={index} value={content.id}>{content.name}</option>
+                            })}
+                        </Select>
+                    }else{
+                        return <Field name={field.name} type={field.type} label={field.name} value={field.value} onChange={this.handleSequenceChange}/>
+                    }
+
                 })}
             </div>
         )
--- src/services/objectApi.js
+++ src/services/objectApi.js
@@ -3,23 +3,23 @@ import {API_URL} from "../config";
 
 
 function getAllObjects() {
-    return axios.post(`${API_URL}objects`, {}).then(response => response.data);
+    return axios.post(`${API_URL}objets`, {}).then(response => response.data.objets);
 }
 
 function getObject(objectId) {
-    return axios.post(`${API_URL}object`, {objectId: objectId}).then(response => response.data);
+    return axios.post(`${API_URL}objet`, {objectId: objectId}).then(response => response.data);
 }
 
 function createObject(object) {
-    return axios.post(`${API_URL}object/create`, {object: object});
+    return axios.post(`${API_URL}objet/create`, {object: object});
 }
 
 function updateObject(objectId, object) {
-    return axios.post(`${API_URL}object/update`, {objectId: objectId, object: object});
+    return axios.post(`${API_URL}objet/update`, {objectId: objectId, object: object});
 }
 
 function deleteObject(objectId) {
-    return axios.post(`${API_URL}object/delete`, {objectId: objectId});
+    return axios.post(`${API_URL}objet/delete`, {objectId: objectId});
 }
 
 export default {
