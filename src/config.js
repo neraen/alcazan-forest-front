ec0fc650da0109f1cc94352bfad103d77d07d404
neraen <clement.silvestre31@gmail.com>
2023-06-25 23:21:01 +0200

    debut world maker

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //export const API_URL = "http://54.38.32.162/api/";//process.env.API_URL;
-export const API_URL = "http://localhost:8000/api/"
+export const API_URL = "http://127.0.0.1:8000/api/"
 export const USER_API = API_URL + "users";
 export const CARTE_API = API_URL + "cartes";
 export const LOGIN_API = API_URL + "login_check";
